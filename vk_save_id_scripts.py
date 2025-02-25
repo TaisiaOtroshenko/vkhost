03eb330090bff291fc8f4c5b77617d14d5da8d7a
Taisia <taya311202@mail.ru>
2025-02-25 11:46:49 +0300

    выбор несуществующих сообщений
    В целом, уже работает, но стоит разделить на две функциональные единицы, чтобы запускать повторно скрипт.

--- vk_save_id_scripts.py
+++ vk_save_id_scripts.py
@@ -1,14 +1,31 @@
 import requests
 import json
 from vk_tokens import *
-# работающая версия параметра "code" из web-редактора 
+
+# работающие примеры параметра "code" из web-редактора 
     # "return[API.messages.getById({"message_ids": "544117,544116","extended":"1"}),API.messages.getById({"message_ids": 544116}).items];"
     # "var cur = '1253100'; var arr = ""; while (cur != '1253000') { arr = arr+cur+","; cur = cur - 1;} return API.messages.getById({"message_ids":arr}).items@.id;"
     # "var cur = 125000; var arr0 = ""; while (cur != 124900) { arr0 = arr0+cur+","; cur = cur - 1;} var cur = 124900; var arr1 = ""; while (cur != 124800) { arr1 = arr1+cur+","; cur = cur - 1;}return[arr0, arr1];"
+#образец генерации ids. работает, не трогай
+#code = 'var cur = 125000; var arr0 = ""; while (cur != 124900) { arr0 = arr0+cur+","; cur = cur - 1;} var cur = 124900; var arr1 = ""; while (cur != 124800) { arr1 = arr1+cur+","; cur = cur - 1;} return[arr0, arr1];'
+#code = code.replace('+','%2b')
+
+#регрессионное тестирование "генерация code"
+'''
+# рукописное создание и запрос двух массивов. 
+# можно вывести для сверки, что я не сломала символы в сненерированном запросе. при command_count = 2 запросы должны идеально совпадать.
+
+code = 'var cur = 125000; var arr0 = ""; while (cur != 124900) { arr0 = arr0+cur+","; cur = cur - 1;} var cur = 124900; var arr1 = ""; while (cur != 124800) { arr1 = arr1+cur+","; cur = cur - 1;}'
+code = code.replace('+','%2b')
+code = code + 'return [API.messages.getById({"message_ids":arr0}).items@.id, API.messages.getById({"message_ids":arr1}).items@.id];'
+'''
+
+
 # сохранение истории сообщений используя vkscripts
+
 id_current = 125000
 count = 100 # 100 максимум
-command_count = 2 # 9 максимум
+command_count = 1 # 5 максимум
 
 
 ids_generate = []
@@ -18,29 +35,35 @@ for i in range(command_count):
     id_first = id_current-i*count
     id_last = id_first - count
     #добавление команд в список
-    ids_generate.append( 'var cur = ' + str(id_first) + '; var ' + arr_name[i] +' = ""; while (cur != ' + str(id_last) + ') { ' + arr_name[i] +' = ' + arr_name[i] +'+cur+","; cur = cur - 1;}')
+    ids_generate.append( 'var cur = ' + str(id_first) + '; var ' + arr_name[i] +' = ""; while (cur != ' + str(id_last) + ') { ' + arr_name[i] +' = ' + arr_name[i] +'+cur+","; cur = cur - 1;} ')
     call_generate.append( 'API.messages.getById({"message_ids":' + arr_name[i] +'}).items@.id')
    
 
 ids_generate = ''.join(ids_generate)
 call_generate = ','.join(call_generate)
+code_generate = f'{ids_generate}return[{call_generate}];' #склеиваем генерацию массивов и запросов
+#print(code_generate) #проверяем что мы спрашиваем
+code_generate = code_generate.replace('+','%2b') #ставим корректные плюсы для интернетной ссылки
 
-#это теоретически правильный запрос
-#r = requests.post(f'https://api.vk.com/method/execute?code={ids_generate}return[{call_generate}];&access_token={access_token_me}&v=5.199').json()
+r = requests.post(f'https://api.vk.com/method/execute?code={code_generate}&access_token={access_token_me}&v=5.199').json()
+#print(r)
 
-#это я запрос тестовый пытаюсь пытаюсь сделать, а оно ошибку дает
-r = requests.post('https://api.vk.com/method/execute?code="var cur = 125000; var arr0 = ""; while (cur != 124900) { arr0 = arr0+cur+","; cur = cur - 1;} var cur = 124900; var arr1 = ""; while (cur != 124800) { arr1 = arr1+cur+","; cur = cur - 1;}return[arr0, arr1];"'+ f'&access_token={access_token_me}&v=5.199').json()
-print('\n', r)
-
-
-
-#запись в .json
-
-'''
-items = []
+#объединение полученных ids в один массив
+response_ids = []
 for i in range(command_count):
-    for x in r['response'][i]:
-        items.append(x)
-            
-json.dump(items,open('scrpt_messages.json','w', encoding='utf-8'))
-'''
+    for x in r['response']:
+        response_ids = response_ids + x
+
+#выбор отстутсвующих
+missing_ids = []
+for i in range(command_count*count):
+    if not(id_current-i in response_ids):
+        missing_ids.append(id_current-i)
+
+#добавление новых ids в файл
+ids = json.load(open('missing_ids.json','r', encoding='utf-8'))
+ids = ids + missing_ids
+json.dump(missing_ids,open('missing_ids.json','w', encoding='utf-8'))
+
+
+
